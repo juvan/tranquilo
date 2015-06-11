3ecb01bf31dca4e3a3bf23c8c13c7fe087e10979
juvan <marcelocolomer76@gmail.com>
2015-06-12 01:59:03 +0200

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tranquilobirdersApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tranquilobirdersApp
+ */
+angular.module('tranquilobirdersApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tranquilobirdersApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tranquilobirdersApp
+ */
+angular.module('tranquilobirdersApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
